990dca459f5fb82260a71e7d56e26b273e34b7ea
Jack Summerfield <summerzj10@gmail.com>
2024-07-13 18:36:43 +0000

    feat: add exercise cls + initialise attributes of the cls

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Exercise:
+    '''
+    Creates an instance of Exercise
+    '''
+    def __init__(self, exercise_type, reps, weight):
+        # Initialises attributes of specific exercise
+        self.exercise_type = exercise_type
+        self.reps = reps
+        self.weight = weight
+
