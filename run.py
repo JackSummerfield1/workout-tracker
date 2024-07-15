66590419040a866be141c0db398d613af9faf312
Jack Summerfield <summerzj10@gmail.com>
2024-07-15 18:08:58 +0000

    fix: format doc, compile menu decoration into 1 func

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ class User:
     '''
     Creates an instance of User
     '''
+
     def __init__(self, username, age, height, weight):
         self.username = username
         self.age = age
@@ -13,6 +14,7 @@ class Exercise:
     '''
     Creates an instance of Exercise
     '''
+
     def __init__(self, exercise_type, sets, reps, weight):
         # Initialises attributes of specific exercise
         self.exercise_type = exercise_type
@@ -25,19 +27,26 @@ class Exercise:
         total_load = self.weight * self.reps
         return total_load
 
-def main():
-    '''
-    Handles all functions and classes
-    '''
+
+def menu_decorator():
     print('*' * 35)
     print('\nWelcome to ASUMA Fitness Tracker\n')
     print('*' * 35)
-    print('\n1. Add Workout\n')
+    print('\nPlease select one of the following options (1-5)...\n')
+    print('1. Add Workout\n')
     print('2. View Workouts\n')
     print('3. Edit Workout\n')
     print('4. Delete Workout\n')
     print('5. Exit\n')
     print('*' * 35)
     # Code above is simply decoration to the terminal menu
+
+
+def main():
+    '''
+    Handles all functions and classes
+    '''
+    menu_decorator()
+
 
 main()
